f2e4a4f2f57c46b4e57144b4f8c13d1c1118dcf5
Charles Hoff <chuck@MacBook-Pro-3.lan>
2024-10-29 01:08:23 -0700

    updated with cash register stuff

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let price = 1.87;
+let price = 5;
 
 let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+  ['PENNY', 1.00],
+  ['NICKEL', 0],
+  ['DIME', 0],
+  ['QUARTER', 0],
+  ['ONE', 2],
+  ['FIVE', 10],
+  ['TEN', 10],
+  ['TWENTY', 0],
+  ['ONE HUNDRED', 0]
 ];
 
 const denoms = [
@@ -32,75 +32,100 @@ const changeDueElement = document.getElementById("change-due");
 
 let cashInDrawer = 0;
 let changeDue = 0;
+let cashTendered = 0;
 
 const calculateCashInDrawer = (array) => {
-
+  cashInDrawer = 0;
   for (let i = 0; i < array.length; i++) {
       cashInDrawer += array[i][1]
     }
-  return cashInDrawer.toFixed(2);
+  cashInDrawer = parseFloat(cashInDrawer.toFixed(2))
 }
 
 const calculateChangeDue = (cashTendered) => {
-  return parseFloat(cashTendered - price).toFixed(2)
+  return parseFloat((cashTendered - price).toFixed(2))
 };
 
-const cannotTenderChange = () => {
-  let firstDenomCount = 0;
-  if (changeDue > 0) {
-    let firstIndex = denoms.findIndex((row) => row[1] <= changeDue);
-    console.log(firstIndex);
-    const firstDenomString = denoms[firstIndex][0];
-    console.log(firstDenomString);
-    const firstDenomValue = denoms[firstIndex][1];
-    for (let i = 1; firstDenomValue * i <= changeDue; i++) {
-      firstDenomCount++;
+const tenderChange = (changeNeeded) => {
+  let denomCountLimit = 0;
+  console.log(cashInDrawer);
+  if (changeNeeded > 0 && cashInDrawer > changeNeeded) {
+    const denomIndex = denoms.findIndex((row) => row[1] <= changeNeeded);
+    console.log(denomIndex);
+    const denomString = denoms[denomIndex][0];
+    console.log(denomString);
+    const denomValue = denoms[denomIndex][1];
+    for (let i = 1; denomValue * i <= changeNeeded; i++) {
+      denomCountLimit++;
     };
-    console.log(firstDenomCount);
-    const firstCidIndex = cid.findIndex((row) => row[0] === firstDenomString)
-    const currentDenomCount = cid[firstCidIndex][1] / denoms[firstIndex][1];
+    console.log(denomCountLimit);
+    const cidIndex = cid.findIndex((row) => row[0] === denomString)
+    const currentDenomCount = cid[cidIndex][1] / denoms[denomIndex][1];
     console.log(currentDenomCount);
-    if (firstDenomCount <= currentDenomCount) {
-      cid[firstCidIndex][1] -= firstDenomValue * firstDenomCount;
+    if (denomCountLimit <= currentDenomCount) {
+      cid[cidIndex][1] -= denomValue * denomCountLimit;
       console.log(cid);
-      let remainder = (changeDue - firstDenomValue * firstDenomCount).toFixed(2);
+      if (changeDueElement.innerHTML === "Change due:") {
+        changeDueElement.innerHTML =
+          `Status: OPEN<div>${denomString}: $${(denomValue * denomCountLimit).toFixed(2)}</div>`;
+      } else {
+        changeDueElement.innerHTML +=
+          `<div>${denomString}: $${(denomValue * denomCountLimit).toFixed(2)}</div>`;
+      }
+      let remainder = parseFloat((changeNeeded - denomValue * denomCountLimit).toFixed(2));
       console.log(remainder);
       if (remainder === 0) {
-        return false;
+        return;
       } else {
-        LOOP
+        tenderChange(remainder)
       }
-    } else if (firstDenomCount > currentDenomCount && currentDenomCount > 0) {
-      cid[firstCidIndex][1] -= firstDenomValue * currentDenomCount;
-      console.log(cid);
-      let remainder = (changeDue - firstDenomValue * currentDenomCount).toFixed(2);
-      console.log(remainder);
+    } else if (denomCountLimit > currentDenomCount && currentDenomCount > 0 && denomIndex !== denoms.length - 1) {
+        cid[cidIndex][1] -= denomValue * currentDenomCount;
+        console.log(cid);
+        if (changeDueElement.innerHTML === "Change due:") {
+          changeDueElement.innerHTML =
+            `Status: OPEN<div>${denomString}: $${(denomValue * currentDenomCount).toFixed(2)}</div>`;
+        } else {
+          changeDueElement.innerHTML +=
+            `<div>${denomString}: $${(denomValue * currentDenomCount).toFixed(2)}</div>`;
+        }
+        let remainder = parseFloat((changeNeeded - denomValue * currentDenomCount).toFixed(2));
+        console.log(remainder);
+        if (remainder === 0) {
+          return;
+        } else {
+        tenderChange(remainder)
+      }
+    } else if (currentDenomCount === 0 && denomIndex !== denoms.length - 1) {
+        denoms.splice(denomIndex, 1);
+        tenderChange(changeNeeded)
+    } else if (denomIndex === denoms.length - 1 && currentDenomCount < denomCountLimit) {
+        changeDueElement.innerHTML = "Status: INSUFFICIENT_FUNDS"
     }
-  } 
-};
-
-const updateChangeDue = () => {
-  if (changeDue === 0) {
-    changeDueElement.innerText = "No change due - customer paid with exact cash"
-  } else if (changeDue < 0) {
-    alert("Customer does not have enough money to purchase the item")
-  } else {
-    if (calculateCashInDrawer(cid) < changeDue) {
-      changeDueElement.innerText = "Status: INSUFFICIENT_FUNDS"
-    } else if (calculateCashInDrawer(cid) === changeDue) {
-      changeDueElement.innerText = "Status: CLOSED"
-    } else if (calculateCashInDrawer(cid) > changeDue) {
-      changeDueElement.innerHTML = ""; 
+  } else if (price > cashTendered) {
+      alert("Customer does not have enough money to purchase the item")
+  } else if (changeNeeded === 0) {
+      changeDueElement.innerText = "No change due - customer paid with exact cash"
+  } else if (changeNeeded === cashInDrawer) {
+      changeDueElement.innerHTML = "Status: CLOSED";
+      for (let i = denoms.length - 1; i > - 1; i--) {
+      if (cid[i][1] !== 0) {
+        changeDueElement.innerHTML += `<div>${cid[i][0]}: $${cid[i][1].toFixed(2)}</div>`
+        }
     }
+  } else if (changeNeeded > cashInDrawer) {
+      changeDueElement.innerText = "Status: INSUFFICIENT_FUNDS";
   }
 }
 
 purchaseBtn.addEventListener("click", () => {
-  const cashTendered = parseFloat(cashTenderedString.value).toFixed(2);
+  changeDueElement.innerHTML = "Change due:";
+  cashTendered = parseFloat((cashTenderedString.value));
   changeDue = calculateChangeDue(cashTendered);
   console.log(changeDue);
-  updateChangeDue();
-  cannotTenderChange();
+  calculateCashInDrawer(cid);
+  console.log(cashInDrawer);
+  tenderChange(changeDue);
 })
 
 console.log(calculateCashInDrawer(cid));
